6036f63ae2ff7684aac7f2259642b11f5574cd33
Aditya jha <iamaditya1202@gmail.com>
2026-07-10 13:59:22 +0530

    Configure settings and requirements for Render deployment

--- gym/myproject/settings.py
+++ gym/myproject/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = "dev-secret-key"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG=True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,6 +132,8 @@ USE_TZ = True
 #     os.path.join(BASE_DIR, 'static'),
 # )
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
